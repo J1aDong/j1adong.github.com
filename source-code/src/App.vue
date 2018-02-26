9c391d3e374be815956e0acd9f59294f64df22fe
J1aDong <991383877@qq.com>
2018-02-26 23:16:51 +0800

    添加点击头像时的动画效果

--- source-code/src/App.vue
+++ source-code/src/App.vue
@@ -1,21 +1,21 @@
 <template>
   <div id="app">
-    <img class="background-img" src="./assets/timg.jpeg">
-    <div class="disc">
+    <img class="background-img" :class="{'background-img-open':open}" src="./assets/timg.jpeg">
+    <div class="disc" @click="discClick" ref="disc">
       <div class="song-img" ref="rotate"></div>
     </div>
-    <div class="lrc" ref="lrc">
+    <div class="lrc" :class="{'lrc-hide':open}" ref="lrc">
       <div v-for="(item,index) in lines" :class="currentLine == index?lrcActive:''">
         {{item.text}}
       </div>
     </div>
-    <audio ref="audio" src="../static/jing.mp3" autoplay loop></audio>
+    <audio ref="audio" src="../static/jing.mp3" @playing="playing" autoplay loop></audio>
   </div>
 </template>
 
 <script>
   import lrc from './assets/song-lrc'
-  import Velocity from 'velocity-animate'
+  import Rebound from 'rebound'
 
   export default {
     name: 'App',
@@ -24,6 +24,12 @@
         lines: [],
         currentLine: 0,
         time: '00:00.00',
+        lrcSpring: {},
+        timer: {},
+
+        open: false,
+
+        discSpring: {}
       }
     },
     computed: {
@@ -34,8 +40,34 @@
     mounted() {
       let audio = this.$refs.audio
       audio.volume = 0.5
-      // console.log(lrc)
-      this.run()
+
+      let that = this
+
+      let springSystem = new Rebound.SpringSystem()
+      this.lrcSpring = springSystem.createSpring(50, 3)
+      this.lrcSpring.addListener({
+        onSpringUpdate: function (spring) {
+          let val = spring.getCurrentValue()
+          that.$refs.lrc.scrollTop = val
+        }
+      })
+
+      this.discSpring = springSystem.createSpring(50, 6)
+      this.discSpring.addListener({
+        onSpringUpdate: function (spring) {
+          let val = spring.getCurrentValue()
+          // console.log('val-->' + val)
+          let discVal = Rebound.MathUtil.mapValueInRange(val, 0, 1, 10, 35)
+          that.$refs.disc.style.marginLeft = discVal + '%'
+          let scaleVal = Rebound.MathUtil.mapValueInRange(val, 0, 1, 0.7, 1)
+          that.$refs.disc.style.transform = "scale(" + scaleVal + ") rotate(45deg)"
+        }
+      })
+
+      this.facLrcs()
+
+      // 转动头像
+      this.$refs.rotate.style.animationPlayState = 'running'
     },
     methods: {
       facLrcs() {
@@ -50,9 +82,9 @@
       },
       run: function () {
         let that = this
-        this.facLrcs()
         let startTime = new Date()
-        let timer = setInterval(function () {
+        clearInterval(this.timer)
+        this.timer = setInterval(function () {
           let currentTime = new Date()
           let diff = new Date(currentTime - startTime)
           let ms = parseInt(diff.getMilliseconds() / 10)
@@ -61,6 +93,13 @@
       },
       pad(x) {
         return (x < 10) ? ("0" + x) : x;
+      },
+      playing() {
+        console.log('playing-->')
+        this.run()
+      },
+      discClick() {
+        this.open = !this.open
       }
     },
     watch: {
@@ -78,11 +117,21 @@
       },
       currentLine(val) {
         if (val > 5) {
-          this.$refs.lrc.scrollTop = (val - 5) * 20
+          this.lrcSpring.setEndValue((val - 5) * 15)
+          // this.$refs.lrc.scrollTop = (val - 5) * 20
           // Velocity(this.$refs.lrc, {scroll: (val - 4) * 20}, {duration: 1000})
         } else {
-          this.$refs.lrc.scrollTop = 0
+          this.lrcSpring.setEndValue(0)
+          // this.$refs.lrc.scrollTop = 0
           // Velocity(this.$refs.lrc, {scroll: 0}, {duration: 1000})
+        }
+      },
+      open(val) {
+        console.log('open-->' + val)
+        if (val) {
+          this.discSpring.setEndValue(1)
+        } else {
+          this.discSpring.setEndValue(0)
         }
       }
     }
@@ -111,28 +160,38 @@
     width 150%
     height auto
     margin-top -10%
-    margin-left -10%;
+    margin-left -10%
     background-size: cover
-    z-index: -1;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    filter: blur(20px);
+    z-index: -1
+    left: 0
+    top: 0
+    right: 0
+    bottom: 0
+    filter: blur(6px)
+    transition 1s ease-in-out
+
+  .background-img-open
+    transform scale(1.2)
+    filter: blur(20px)
 
   .disc
     position relative
-    margin-top 30% - 10rem
-    margin-left 50% - 10rem
+    margin-top 10%
+    margin-left 10%
     width disc-width
     height disc-width
     border-radius disc-width
-    transform rotate(45deg)
+    transform scale(0.7) rotate(45deg)
     background black
     background-image radial-gradient(5em 30em ellipse, #fff, #000)
     border 2px solid #131313
     box-shadow 0 0 0 10px #343935
     opacity 0.7
+
+  .disc-open
+    margin-top 15%
+    margin-left 30%
+    transform scale(1) rotate(45deg)
 
   @keyframes animations1
     0%
@@ -164,12 +223,18 @@
     right 10%
     top 10%
     overflow auto
-    width 20%
+    width 40%
     height 50%
     color white
+    transition 1s ease-in-out
+
+  .lrc-hide
+    right 2%
+    opacity 0
 
   .lrc-active
     color red
     font-size 20px
     transition 1s ease-in-out
+
 </style>
