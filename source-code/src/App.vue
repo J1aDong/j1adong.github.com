d0a029daf2c7a3cbf13c886963ec206086a2ff1e
J1aDong <991383877@qq.com>
2018-02-27 19:58:43 +0800

    添加一些细节

--- source-code/src/App.vue
+++ source-code/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img class="background-img" :class="{'background-img-open':open}" src="./assets/timg.jpeg">
+    <img class="background-img" :class="{'background-img-open':open}" src="./assets/jing.png">
     <div class="disc" @click="discClick" ref="disc">
       <div class="song-img" ref="rotate"></div>
     </div>
@@ -9,7 +9,19 @@
         {{item.text}}
       </div>
     </div>
+    <a class="mj-title" :class="{'mj-title-open':open}" href="https://github.com/J1aDong" target="_blank">
+      <icon name="github" scale="1.5"></icon>
+      J1aDong.github.com</a>
+    <div class="home-wrapper" :class="{'home-wrapper-open':open}" @mouseenter="homeHoverFun(true)"
+         @mouseleave="homeHoverFun(false)">
+      <span class="home" :class="{'home-hover':homeHover}"></span>
+      <icon class="home-icon" :class="{'home-icon-hover':homeHover}" name="chevron-circle-up" color="white"></icon>
+      <a class="home-content" :class="{'home-content-hover':homeHover}" href="http://47.96.31.106:32771/#/"
+         target="_blank">我的空间站</a>
+    </div>
+    <div class="content" :class="{'content-open':open}">第五十天，好好的活下去</div>
     <audio ref="audio" src="../static/jing.mp3" @playing="playing" autoplay loop></audio>
+    <input class="voice" ref="voiceRange" type="range" v-model="voice" :max="100" min="0">
   </div>
 </template>
 
@@ -29,7 +41,11 @@
 
         open: false,
 
-        discSpring: {}
+        discSpring: {},
+
+        homeHover: false,
+
+        voice: 50
       }
     },
     computed: {
@@ -38,8 +54,12 @@
       }
     },
     mounted() {
-      let audio = this.$refs.audio
-      audio.volume = 0.5
+      // let audio = this.$refs.audio
+      // audio.volume = 0.5
+
+      this.voice = localStorage.getItem('voice') || 100
+
+      this.$refs.voiceRange.style.background = `linear-gradient(to right, #eb7470, #eb7470 ${this.voice}%, white ${this.voice}%, white)`
 
       let that = this
 
@@ -100,6 +120,9 @@
       },
       discClick() {
         this.open = !this.open
+      },
+      homeHoverFun(val) {
+        this.homeHover = val
       }
     },
     watch: {
@@ -117,7 +140,7 @@
       },
       currentLine(val) {
         if (val > 5) {
-          this.lrcSpring.setEndValue((val - 5) * 15)
+          this.lrcSpring.setEndValue((val - 5) * 18)
           // this.$refs.lrc.scrollTop = (val - 5) * 20
           // Velocity(this.$refs.lrc, {scroll: (val - 4) * 20}, {duration: 1000})
         } else {
@@ -133,6 +156,13 @@
         } else {
           this.discSpring.setEndValue(0)
         }
+      },
+      voice() {
+        console.log("voice-->" + this.voice)
+        this.$refs.audio.volume = this.voice / 100
+        this.$refs.voiceRange.style.background = `linear-gradient(to right, #eb7470, #eb7470 ${this.voice}%, white ${this.voice}%, white)`
+
+        localStorage.setItem("voice", this.voice)
       }
     }
   }
@@ -141,6 +171,7 @@
 <style lang="stylus" type="text/stylus">
   disc-width = 25rem
   head-icon-width = 15rem
+  home-button-width = 6rem
 
   #app
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -157,22 +188,20 @@
 
   .background-img
     position absolute
-    width 150%
+    width 120%
     height auto
     margin-top -10%
     margin-left -10%
     background-size: cover
-    z-index: -1
     left: 0
     top: 0
     right: 0
     bottom: 0
-    filter: blur(6px)
-    transition 1s ease-in-out
+    filter: blur(10px)
+    transition transform 2s ease-in-out
 
   .background-img-open
     transform scale(1.2)
-    filter: blur(20px)
 
   .disc
     position relative
@@ -187,11 +216,18 @@
     border 2px solid #131313
     box-shadow 0 0 0 10px #343935
     opacity 0.7
+    cursor pointer
 
   .disc-open
     margin-top 15%
     margin-left 30%
     transform scale(1) rotate(45deg)
+
+  input[type=range]
+    -webkit-appearance: none
+    height: 8px
+    border-radius: 10px
+    background-color: #fff
 
   @keyframes animations1
     0%
@@ -234,7 +270,107 @@
 
   .lrc-active
     color red
-    font-size 20px
+    font-size 1.6rem
     transition 1s ease-in-out
+
+  .content
+    position absolute
+    bottom -2rem
+    width 100%
+    color white
+    opacity 0.1
+    transition 1s ease-in-out
+
+  .content-open
+    bottom 3rem
+    opacity 1
+    font-size 1.5rem
+    transform rotateX(-360deg)
+
+  .mj-title
+    position fixed
+    color rgba(178, 178, 178, 0.98)
+    bottom 2rem
+    left 10rem
+    text-decoration none
+    font-size 2rem
+    transition 0.5s ease-in-out
+
+  .mj-title-open
+    font-size 1rem
+
+  .home-wrapper
+    position relative
+    top 400px
+    background-color #2c3e50
+    opacity 1
+    transition 1s ease-in-out
+    visibility hidden
+    transform rotateY(-90deg)
+
+  .home-wrapper-open
+    opacity 1
+    top 200px
+    visibility visible
+    transform rotateY(0)
+
+  .home
+    position absolute
+    left 50% - (home-button-width/ 2)
+    bottom 20%
+    width home-button-width
+    height home-button-width
+    border-radius home-button-width
+    background-color rgba(0, 0, 0, 0.25)
+    cursor pointer
+    box-shadow 0 0 10px 1px rgba(0, 0, 0, 0.55)
+    transition 0.3s ease-in-out
+
+  .home-icon
+    position absolute
+    width 3rem
+    height 3rem
+    border-radius 3rem
+    bottom 1.5rem
+    left 50% - 1.3rem
+    transition 0.5s ease-in-out
+
+  .home-icon-hover
+    left 50% - 6rem
+    transform rotate(90deg)
+
+  .home-hover
+    position absolute
+    left 50% - (home-button-width/ 2) - 5rem
+    bottom 22%
+    width home-button-width + 10rem
+    height home-button-width
+    border-radius home-button-width
+    background-color rgba(0, 0, 0, 0.45)
+    box-shadow 0 0 100px 1px rgba(0, 0, 0, 0.55)
+    text-align left
+    line-height home-button-width
+    cursor pointer
+
+  .home-content
+    position absolute
+    left 50%
+    height 3.5rem
+    opacity 0
+    bottom 10%
+    text-decoration none
+    color #ef0f4e
+    transition 0.2s ease-in-out
+
+  .home-content-hover
+    opacity 1
+    font-size 1.2rem
+
+  .voice
+    position fixed
+    bottom 5rem
+    right 10rem
+    width 15rem
+
 
 </style>
